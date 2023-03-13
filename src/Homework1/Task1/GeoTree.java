78cd816ccd396ba98bc860540a2968bda2a7ece3
Djirgal <djirgal.poshtarova@yandex.ru>
2023-03-13 20:00:05 +0300

    Task1 is re-done

--- src/Homework1/Task1/GeoTree.java
+++ src/Homework1/Task1/GeoTree.java
@@ -2,7 +2,7 @@ package Homework1.Task1;
 
 import java.util.ArrayList;
 
-public class GeoTree {
+public class GeoTree implements Research{
 
     private ArrayList<Node> tree = new ArrayList<>();
 
@@ -27,5 +27,41 @@ public class GeoTree {
         tree.add(new Node(partner1,Relationship.partner,partner2));
         tree.add(new Node(partner2,Relationship.partner,partner1));
 
+    }
+
+    @Override
+    public ArrayList<String> getParents(Person person) {
+        System.out.printf("Родители человека по имени %s : ", person.getFullName());
+        return spend(person, Relationship.children);
+    }
+
+    @Override
+    public ArrayList<String> getChildren(Person person) {
+        System.out.printf("Дети человека по имени %s : ", person.getFullName());
+        return spend(person, Relationship.parent);
+    }
+
+    @Override
+    public void getPartners(Person person) {
+        ArrayList<String> result = spend(person, Relationship.partner);
+        if (result.isEmpty()) {
+            System.out.printf("Данных о супруге человека по имени %s нет.", person.getFullName());
+        }
+        else {
+            System.out.printf("Супруг/супруга человека по имени %s : ", person.getFullName());
+            System.out.println(result);
+
+        }
+    }
+
+    @Override
+    public ArrayList<String> spend(Person p, Relationship relation) {
+        ArrayList<String> result = new ArrayList<>();
+        for (Node t : tree) {
+            if (t.p1.getFullName().equals(p.getFullName()) && t.getRelationship() == relation) {
+                result.add(t.p2.getFullName());
+            }
+        }
+        return result;
     }
 }
--- src/Homework1/Task1/Research.java
+++ src/Homework1/Task1/Research.java
@@ -2,21 +2,13 @@ package Homework1.Task1;
 
 import java.util.ArrayList;
 
-public class Research {
-    ArrayList<String> result = new ArrayList<>();
-    ArrayList<Node> tree;
+public interface Research {
 
-    public Research(GeoTree geoTree) {
-        tree = geoTree.getTree();
-    }
+    ArrayList<String> getParents(Person person);
 
-    public ArrayList<String> spend(Person p, Relationship relation) {
-        for (Node t : tree) {
-            if (t.p1.getFullName().equals(p.getFullName()) && t.getRelationship() == relation) {
-                result.add(t.p2.getFullName());
-            }
-        }
-        return result;
-    }
+    ArrayList<String> getChildren(Person person);
 
+    void getPartners(Person person);
+
+    ArrayList<String> spend(Person p, Relationship relation);
 }
