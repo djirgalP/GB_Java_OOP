2b590cdb16cd27cbb17191c0b2a8d39c69dfce44
Djirgal <djirgal.poshtarova@yandex.ru>
2023-03-27 17:28:06 +0300

    Тажке для класса Contact переопределен метод hashCode()

--- src/Homework5/Core/Models/Contact.java
+++ src/Homework5/Core/Models/Contact.java
@@ -71,4 +71,13 @@ public class Contact implements Comparable<Contact>{
         Contact t = (Contact)obj;
         return (this.firstName.equals(t.firstName) && this.lastName.equals(t.lastName));
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
 }
